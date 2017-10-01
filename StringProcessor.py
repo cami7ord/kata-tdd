ee83bf8e6430b2c8aa812909cbf675f1d447f9ec
cami7ord <cami7ord@gmail.com>
2017-10-01 02:16:20 -0500

    Step 5. Fix the process method to pass with a string of one number. (Green)

--- StringProcessor.py
+++ StringProcessor.py
@@ -42,3 +42,5 @@ class StringProcessor:
     def process(self, string):
         if string == "":
             return [0, 0, 0, 0]
+        else:
+            return [1, int(string), int(string), int(string)]
