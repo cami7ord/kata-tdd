c38c2c91e3083f6dd8316b03edea4e12e08c0073
cami7ord <cami7ord@gmail.com>
2017-10-01 00:14:52 -0500

    Step 1. Test the length method to fail with a string of one number. (Red)

--- TestStringProcessor.py
+++ TestStringProcessor.py
@@ -4,5 +4,8 @@ from StringProcessor import StringProcessor
 
 class TestStringProcessor(TestCase):
 
-    def test_length(self):
+    def test_length_empty(self):
         self.assertEqual(StringProcessor().length(""), 0, "Length of empty string.")
+
+    def test_length_one(self):
+        self.assertEqual(StringProcessor().length("5"), 1, "Length of one number.")
