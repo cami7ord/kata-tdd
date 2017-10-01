08a85c9b8722fabcbc4dcc60ab4ec7a62f0d6b06
cami7ord <cami7ord@gmail.com>
2017-10-01 00:52:56 -0500

    Step 1. Refactor to use a single instance of the StringProcessor, end of Step 1. (Blue)

--- TestStringProcessor.py
+++ TestStringProcessor.py
@@ -4,14 +4,20 @@ from StringProcessor import StringProcessor
 
 class TestStringProcessor(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.sp = StringProcessor()
+
+    #Length
+
     def test_length_empty(self):
-        self.assertEqual(StringProcessor().length(""), 0, "Length of empty string.")
+        self.assertEqual(self.sp.length(""), 0, "Length of empty string.")
 
     def test_length_one(self):
-        self.assertEqual(StringProcessor().length("5"), 1, "Length of one number.")
+        self.assertEqual(self.sp.length("5"), 1, "Length of one number.")
 
     def test_length_two(self):
-        self.assertEqual(StringProcessor().length("5,3"), 2, "Length of two numbers.")
+        self.assertEqual(self.sp.length("5,3"), 2, "Length of two numbers.")
 
     def test_length_n(self):
-        self.assertEqual(StringProcessor().length("5,3,4,1,6,8"), 6, "Length of six numbers.")
+        self.assertEqual(self.sp.length("5,3,4,1,6,8"), 6, "Length of six numbers.")
