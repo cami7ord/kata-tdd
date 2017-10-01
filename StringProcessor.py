819e4b392b1b259c4d24c0e176880880e838dd48
cami7ord <cami7ord@gmail.com>
2017-10-01 00:34:17 -0500

    Step 1. Changed the code to pass length on a string with two numbers. (Green)

--- StringProcessor.py
+++ StringProcessor.py
@@ -3,5 +3,7 @@ class StringProcessor:
     def length(self, string):
         if string == "":
             return 0
+        elif "," in string:
+            return 2
         else:
             return 1
